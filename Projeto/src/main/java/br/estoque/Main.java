36b9a2632e16fc5f350c126d1c07f2ddc08665eb
layssasuelen03-criador <layssasuelen03@gmail.com>
2025-11-23 21:19:33 -0300

    AT3 - Projeto final

--- Projeto/src/main/java/br/estoque/Main.java
+++
@@ -1,6 +0,0 @@
-package br.estoque;
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Sistema Estoque Maven pronto!");
-    }
-}
--- Projeto/src/main/java/br/estoque/model/Categoria.java
+++
@@ -1,8 +0,0 @@
-package br.estoque.model;
-public class Categoria{
-    private int id; private String nome; private String descricao;
-    public Categoria(String nome,String descricao){this.nome=nome;this.descricao=descricao;}
-    public int getId(){return id;} public void setId(int id){this.id=id;}
-    public String getNome(){return nome;} public String getDescricao(){return descricao;}
-    public void setNome(String n){nome=n;} public void setDescricao(String d){descricao=d;}
-}
